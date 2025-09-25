f4572221fbc4f21a2eacb142f56bbd3612d0bacf
Kate Korobova <korkat1611@gmail.com>
2025-09-25 15:15:03 +0300

    add final_balance

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-int ticket_price = 10_000;
-int rubles_for_one_bonus = 20;
-int milli = ticket_price / rubles_for_one_bonus;
-        System.out.println(milli);
 
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной милли
-
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        int balance = 100;
+        int deposit_amount = 1100;
+        int limit = 1000;
+        int bonus_rate = 1;
+        int bonus_step = 100;
+        int bonus = 0;
+        if (deposit_amount >= limit) {
+            bonus = (deposit_amount / bonus_step) * bonus_rate;
+        }
+        int final_balance = balance + deposit_amount + bonus;
+        System.out.println(final_balance);
     }
 }
