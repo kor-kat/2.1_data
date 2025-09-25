4ff44fbc2393acebd98f11b24a5603c3a6cd5aea
Kate Korobova <korkat1611@gmail.com>
2025-09-25 14:32:29 +0300

    add milli

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
-public class MainV1 {
+public class Main {
     public static void main(String[] args) {
-        boolean registered = true;
-        float amount = 1000.60F;
-        float percent = 0.03F;
+int ticket_price = 10_000;
+int rubles_for_one_bonus = 20;
+int milli = ticket_price / rubles_for_one_bonus;
+        System.out.println(milli);
 
-        float bonus = amount * percent;
-        System.out.println(bonus);
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной милли
+
+        // Рассчитываете количество бонусных милль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
     }
 }
