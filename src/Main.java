5c95a0fc798d08fb94af17ce1058d297bddb8c3a
Kate Korobova <korkat1611@gmail.com>
2025-09-26 10:06:03 +0300

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-int ticket_price = 10_000;
-int rubles_for_one_bonus = 20;
-int milli = ticket_price / rubles_for_one_bonus;
+        double ticketPrice = 9999.99;
+        int rublesForOneBonus = 20;
+        int milli = (int) Math.floor(ticketPrice / rublesForOneBonus);
         System.out.println(milli);
-
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной милли
-
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
     }
 }
